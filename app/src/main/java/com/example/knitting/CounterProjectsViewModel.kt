336c83caf089a1359421a781b03caa1e6d8ef506
Dimkaaaaa <thisnamebusy@gmail.com>
2020-11-22 18:00:17 +0300

    Adding new layout - counter_projects_fragment.xml

---
+++ app/src/main/java/com/example/knitting/CounterProjectsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.knitting
+
+import androidx.lifecycle.ViewModel
+
+class CounterProjectsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/knitting/MainMenuFragment.kt
+++ app/src/main/java/com/example/knitting/MainMenuFragment.kt
@@ -5,14 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 
 class MainMenuFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainMenuFragment()
-    }
-
     private lateinit var viewModel: MainMenuViewModel
 
     override fun onCreateView(
@@ -20,12 +14,6 @@ class MainMenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.main_menu_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainMenuViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
