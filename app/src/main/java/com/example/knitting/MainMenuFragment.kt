7e6eb25b1166d27edfe20611ae6f1b8efd958f8a
Dimkaaaaa <thisnamebusy@gmail.com>
2020-11-22 17:36:36 +0300

    First launch, refactoring.

---
+++ app/src/main/java/com/example/knitting/MainMenuFragment.kt
@@ -0,0 +1,31 @@
+package com.example.knitting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class MainMenuFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainMenuFragment()
+    }
+
+    private lateinit var viewModel: MainMenuViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_menu_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainMenuViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
