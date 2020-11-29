8c0fa6b57d91e37de5ea3710bc3b009615e5fc19
Dimkaaaaa <thisnamebusy@gmail.com>
2020-11-29 16:35:55 +0300

    Adding Database for Room

---
+++ app/src/main/java/com/example/knitting/database/CounterDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.knitting.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Counter::class], version = 1)
+abstract class CounterDatabase : RoomDatabase() {
+
+
+    abstract val counterDAO: CounterDAO
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: CounterDatabase? = null
+
+        fun getInstance(context: Context): CounterDatabase {
+
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context,
+                            CounterDatabase::class.java,
+                            "counter_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
